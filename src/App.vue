8a09fbed4a2357f3ff79158a0f1b47b9ab0d901a
DanioFiore <daniofiore@hotmail.com>
2023-01-25 10:56:42 +0100

    project setup

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <ul>
+        
+    </ul>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            storedResources: [
+                {
+                    id: 'vue-guide',
+                    title: 'Vue Official Guide',
+                    description: 'The official guide of vue.js',
+                    link: 'https://vuejs.org',
+                },
+                {
+                    id: 'google',
+                    title: 'Google',
+                    description: 'Learn to google',
+                    link: 'https://google.com',
+                }
+            ]
+        }
+    }
+}
+</script>
