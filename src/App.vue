0852f51be0afa97938af75b4137b60d6c12dedc0
DanioFiore <daniofiore@hotmail.com>
2023-01-25 11:45:12 +0100

    added the learning resource component that is used to show the storedResources data from App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
     <ul>
-        
+        <learning-resource v-for="resource in storedResources" :key="resource.id" :id="resource.id" :title="resource.title" :description="resource.description" :link="resource.link"></learning-resource>
     </ul>
 </template>
 
 <script>
+import LearningResource from './components/learning-resources/LearningResource.vue';
+
 export default {
+    components: {
+        LearningResource,
+    },
     data() {
         return {
             storedResources: [
