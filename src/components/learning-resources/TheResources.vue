23cfbbfa0d93addecbcbeb6f440f85203e80e948
DanioFiore <daniofiore@hotmail.com>
2023-01-25 14:48:27 +0100

    added the form that allows me to insert a new resources, capture the insert datas and push them into the array of resources

--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -1,13 +1,19 @@
 <template>
   <base-card>
-    <base-button @click="setSelectedTab('stored-resources')"
-      :mode="storedResButtonMode">Stored Resources</base-button
+    <base-button
+      @click="setSelectedTab('stored-resources')"
+      :mode="storedResButtonMode"
+      >Stored Resources</base-button
     >
-    <base-button @click="setSelectedTab('add-resource')"
-      :mode="addResButtonMode">Add Resource</base-button
+    <base-button
+      @click="setSelectedTab('add-resource')"
+      :mode="addResButtonMode"
+      >Add Resource</base-button
     >
   </base-card>
-  <component :is="selectedTab"></component>
+  <keep-alive>
+    <component :is="selectedTab" @adding-resource="addResource"></component>
+  </keep-alive>
 </template>
 
 <script>
@@ -15,30 +21,30 @@ import StoredResources from './StoredResources.vue';
 import AddResource from './AddResource.vue';
 
 export default {
-    computed: {
-        storedResButtonMode() {
-            return this.selectedTab === 'stored-resources' ? null : 'flat';
-        },
-        addResButtonMode() {
-            return this.selectedTab === 'add-resource' ? null : 'flat';
-        }
+  computed: {
+    storedResButtonMode() {
+      return this.selectedTab === 'stored-resources' ? null : 'flat';
     },
-    components: {
-        StoredResources,
-        AddResource,
+    addResButtonMode() {
+      return this.selectedTab === 'add-resource' ? null : 'flat';
     },
+  },
+  components: {
+    StoredResources,
+    AddResource,
+  },
   data() {
     return {
       selectedTab: 'stored-resources',
       storedResources: [
         {
-          id: 'vue-guide',
+          id: 1,
           title: 'Vue Official Guide',
           description: 'The official guide of vue.js',
           link: 'https://vuejs.org',
         },
         {
-          id: 'google',
+          id: 2,
           title: 'Google',
           description: 'Learn to google',
           link: 'https://google.com',
@@ -49,13 +55,24 @@ export default {
 
   provide() {
     return {
-        resources: this.storedResources,
-    }
+      resources: this.storedResources,
+      addResource: this.addResource,
+    };
   },
-  
+
   methods: {
     setSelectedTab(tab) {
       this.selectedTab = tab;
+    },
+
+    addResource(id, title, description, link) {
+      this.storedResources.unshift({
+        id: id,
+        title: title,
+        description: description,
+        link: link,
+      });
+      this.selectedTab = 'stored-resources';
     },
   },
 };
